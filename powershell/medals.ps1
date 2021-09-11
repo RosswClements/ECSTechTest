4cc7827e2a62053648d5ea6d42cbecf487629a40
RosswClements <RosswClements@hotmail.co.uk>
2021-09-11 21:48:34 +0100

    medals.ps1 makes correct table, pester isnt happy

--- powershell/medals.ps1
+++ powershell/medals.ps1
@@ -1,3 +1,5 @@
+#for write-output purposes
+
 $MedalResults = @(
     @{
         'sport' = 'cycling'
@@ -29,21 +31,83 @@ function Build-MedalTable {
     # Create a medal table based on the provided result list
     # First place gives 3 points, second place 2 points and third place 1 point
 
+    $SplitMedals = @()
+
     #Split string on . into key value pairs
 
     For($i = 0 ; $i -lt $Medals.length; $i++) {
         For($ii = 0 ; $ii -lt $Medals[$i]['podium'].length; $ii++) {
-            Write-output $Medals[$i]['podium'][$ii]
-            write-output ''
+            [System.Collections.ArrayList]$SplitMedals += @($Medals[$i]['podium'][$ii].Split(".")[1] , $Medals[$i]['podium'][$ii].Split(".")[0])
+           
             
         }
-    }
-    
-    write-output $Medals
-    #Turn value (country) into key of new table
 
-    #Change medal positions into scores and assign to values of new table
+        
+    }
+   # Write-output $SplitMedals
+        
+    #write-output ''
+
+    For($i = 0 ; $i -lt $SplitMedals.count; $i++) {
+
+        
+        If( $SplitMedals[$i] -eq '1') {
+            $SplitMedals[$i] = 3
+        }
+        elseif( $SplitMedals[$i] -eq '2') {
+            $SplitMedals[$i]  = 2
+        }
+        elseif ( $SplitMedals[$i]  -eq '3') {
+            $SplitMedals[$i]  = 1
+        }
+    }
+   # Write-output $SplitMedals
+
+    For($i = 0 ; $i -lt $SplitMedals.count; $i = $i + 2) {
+        For($j = $i + 2 ; $j -lt $SplitMedals.count; $j = $j + 2) {
+        if($SplitMedals[$i] -eq $SplitMedals[$j]){
+           $SplitMedals[$i+1] += $SplitMedals[$j+1] 
+           $SplitMedals[$j] = 'remove'
+           $SplitMedals[$j+1] = 'remove'
+        }
+
+        }
+    }
+
+  #  Write-output $SplitMedals
+   # Write-output ''
+
+    while ($SplitMedals -contains "remove") {
+        $SplitMedals.Remove("remove")
+        $SplitMedals.Remove("removeremove")
+    }
+   # Write-output $SplitMedals
+  #  Write-output ''
+
+
+    $MedalTable = new-object System.Collections.Hashtable
+    for ( $i = 0; $i -lt $SplitMedals.count; $i += 2 ) {
+    $MedalTable.Add($SplitMedals[$i],$SplitMedals[$i+1]);
 }
 
+  #  Write-output $MedalTable
+ #   Write-output ''
 
+    #write-output $MedalTable
+    return $MedalTable.GetEnumerator() | Sort-Object value -descending
+
+   
+     
+    
+    
+    #write-output $Medals
+    
+    #CommandNotFoundException: The term 'Build-MedalTable' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the spelling of the name, or if a path was included, verify that the path is correct and try again.
+    #Error seems to be outside the 'code goes here' part of the file so not sure what I actually need to do to fix this
+    #Also I'm presumably not supposed to edit the medals.tests.ps1 file?
+}
+
+Import-Module .\medals.ps1
+
+#For testing purposes
 Build-MedalTable $MedalResults
